16fbad67f585584218889f837b7b833b5b76226c
Joe Morana <josephmorana@aol.com>
2014-11-26 18:48:28 -0500

    delete the comment

--- app/src/main/java/com/example/josh/qcmapit/SearchSuggestionList.java
+++ app/src/main/java/com/example/josh/qcmapit/SearchSuggestionList.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
  * Handles the suggestion list
  */
 public class SearchSuggestionList {
-    //Learn to pull!
     public static final String[] buildingList = new String[] {
             "Alumni Hall",
             "Alumni Plaza",
